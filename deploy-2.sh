023509f3a3a14493d3bc2e3f3c3d476c024c1388
Sergey Khmelevskoy <xs290@me.com>
2023-05-23 20:58:38 +0300

    clippathfix

---
+++ deploy-2.sh
@@ -0,0 +1,10 @@
+ssh root@195.2.75.40 << EOF
+  cd /home/nobody.su
+  git checkout main
+  git stash
+  git pull
+  npm i
+  npm run build
+  pm2 stop all
+  pm2 start ecosystem.config.cjs
+EOF
--- src/components/Product/ProductCard.tsx
+++ src/components/Product/ProductCard.tsx
@@ -23,7 +23,12 @@ const ProductCard = ({ name, images, price, salePrice, id }: IProductCardProps) 
       >
         {images?.length > 0 && (
           <div className="product-card__image" style={{ clipPath: settings.itemClip }}>
-            <img src={images[0]} alt={name} loading="lazy" />
+            <img
+              src={images[0]}
+              alt={name}
+              loading="lazy"
+              style={{ clipPath: settings.itemClip }}
+            />
           </div>
         )}
       </div>
